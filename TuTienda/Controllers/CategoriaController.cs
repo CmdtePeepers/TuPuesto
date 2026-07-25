45d34319f22650eecabe0e4b0928be9154ba02cc
Joan Espinoza <espinozajaon@gmail.com>
2026-07-25 08:00:44 -0500

    Se agrego el controllador de categoria y vistas

---
+++ TuTienda/Controllers/CategoriaController.cs
@@ -0,0 +1,151 @@
+﻿using Microusing Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TuTienda.Models.Entities;
+using TuTienda.Data;
+
+namespace TuTienda.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categoria
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categorias.ToListAsync());
+        }
+
+        // GET: Categoria/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoria = await _context.Categorias
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria);
+        }
+
+        // GET: Categoria/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categoria/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Descripcion")] Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categoria);
+                await _context.SaveChangesAsync();
+                TempData["Mensaje"] = $"Categoría {categoria.Nombre} registrada correctamente.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
+        // GET: Categoria/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoria = await _context.Categorias.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+
+        // POST: Categoria/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, [Bind("Id,Nombre,Descripcion")] Categoria categoria)
+        {
+            if (id != categoria.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categoria);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoriaExists(categoria.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
+        // GET: Categoria/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoria = await _context.Categorias
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria);
+        }
+
+       
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
+            if (categoria != null)
+            {
+                _context.Categorias.Remove(categoria);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        private bool CategoriaExists(int id)
+        {
+            return _context.Categorias.Any(e => e.Id == id);
+        }
+    }
+}
